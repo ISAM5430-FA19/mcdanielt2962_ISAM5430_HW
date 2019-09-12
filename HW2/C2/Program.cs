b3aa0e55b3f667f9c0244f6f342bf0c348892c00
Terry <tmcdaniel787@gmail.com>
2019-09-12 16:47:52 -0500

    Updated up to C3

---
+++ HW2/C2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("C.2.8");
+            int start = 1;
+            while (start < 100)
+            {
+                WriteLine(start);
+                start *= 2;
+            }
+            WriteLine("C.2.9");
+            int start9 = 100;
+            while (start9>=1)
+            {
+                WriteLine(start9);
+                start9 /= 2;
+            }
+            WriteLine("0");
+
+            WriteLine("C.2.10");
+            double start10 = 100;
+            while (start10>=1)
+            {
+                WriteLine(start10);
+                start10 /= 2;
+            }
+        }
+    }
+}
---
+++ HW2/C5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("C.3.11");
+            int num11 = 2;
+            int prvnum11 = 1;
+            int total11 = 1;
+            int max = 0;
+            
+            while (max<1000)
+            {                
+                while(prvnum11>=2)
+                {
+                    
+                    total11 = total11 * prvnum11;                    
+                    prvnum11 -= 1;
+                                        
+                }
+                if(total11<1000)
+                {
+                    WriteLine(total11);
+                }
+                
+                max = total11;
+                num11 += 1;
+                total11 = num11;
+                prvnum11 = num11 - 1;                
+                
+            }
+
+            WriteLine("C.3.12");
+            
+            int sum12 = 0;
+            int prvnum1 = 0;
+            int prvnum2 = 1;
+            int count12 = 0;
+            WriteLine(prvnum1);
+            WriteLine(prvnum2);
+            count12 += 2;
+            while (count12<25)
+            {
+                sum12 = prvnum1 + prvnum2;
+                prvnum1 = prvnum2;
+                prvnum2 = sum12;
+                count12 += 1;
+                WriteLine(sum12);
+            }
+            
+        }
+    }
+}
