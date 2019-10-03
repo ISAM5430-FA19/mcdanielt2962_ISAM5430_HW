1c1ee45e4a3cebf90fadb4ce51835266c631f892
tmcdaniel787 <54695239+tmcdaniel787@users.noreply.github.com>
2019-10-03 16:19:32 -0500

    Adding Divisible by 2d.

--- HW4/C/Program.cs
+++ HW4/C/Program.cs
@@ -11,6 +11,19 @@ namespace C
     {
         static void Main(string[] args)
         {
+            WriteLine("Please enter a number.");
+            int n = Convert.ToInt32(ReadLine());
+            WriteLine("Please enter another number.");
+            int d= Convert.ToInt32(ReadLine());
+            int z = 1;
+            for (int i=1;i<=d;i++)
+            {
+                z = z * 10;
+            }            
+            if((n%z)%2 ==0)
+            {
+                WriteLine("The number is divisable");
+            }
 
         }
     }
