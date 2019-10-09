6c110c12764c5b8b419448d4c36466e00aafc715
Terry <tmcdaniel787@gmail.com>
2019-10-09 02:00:03 -0500

    Updated with C2

---
+++ HW5/C.2/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C._2
+{
+    class Motorway
+    {
+        private string _name;
+        private string _type;
+        private string _direction;
+        private string _surface;
+        private int _lanes;
+        private string _toll;
+        private string _mantParty;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+        public string Direction
+        {
+            get
+            {
+                return _direction;
+            }
+            set
+            {
+                _direction = value;
+            }
+        }
+        public string Surface
+        {
+            get
+            {
+                return _surface;
+            }
+            set
+            {
+                _surface = value;
+            }
+        }
+        public int Lanes
+        {
+            get
+            {
+                return _lanes;
+            }
+            set
+            {
+                _lanes = value;
+            }
+        }
+        public string Toll
+        {
+            get
+            {
+                return _toll;
+            }
+            set
+            {
+                _toll = value;
+            }
+        }
+        public string MantainingParty
+        {
+            get
+            {
+                return _mantParty;
+            }
+            set
+            {
+                _mantParty = value;
+            }
+        }
+        public override string ToString()
+        {
+            return ($"Direction-{Direction}, Name-{Name}, Type-{Type}, Surface-{Surface}, Toll-{Toll}, Maintaining Party-{MantainingParty}");
+        }
+    }
+}
---
+++ HW5/C.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorway myMotorway = new Motorway();
+            Console.Write("Enter the name of the motorway: ");
+            string myName = Console.ReadLine();
+            myMotorway.Name = myName;
+            Console.Write("Enter the type of motorway: ");
+            string myType = Console.ReadLine();
+            myMotorway.Type = myType;
+            Console.Write("Enter the direction: ");
+            string myDirection = Console.ReadLine();
+            myMotorway.Direction = myDirection;
+            Console.Write("Enter the surface type: ");
+            string mySurface = Console.ReadLine();
+            myMotorway.Surface = mySurface;
+            Console.Write("Enter number of lanes: ");
+            int myLanes = Convert.ToInt32(Console.ReadLine());
+            myMotorway.Lanes = myLanes;
+            Console.Write("Toll or No Toll? ");
+            string myToll = Console.ReadLine();
+            myMotorway.Toll = myToll;
+            Console.Write("Who maintains the motorway? ");
+            string myMaint = Console.ReadLine();
+            myMotorway.MantainingParty = myMaint;
+            Console.WriteLine($"The name of the roadway is {myMotorway.Direction} {myMotorway.Name} {myMotorway.Type}");
+            Console.WriteLine($"The name of the roadway is {myMotorway.Direction} {myMotorway.Name} {myMotorway.Type}, {myMotorway.Toll}");
+            Console.WriteLine($"The name of the roadway is {myMotorway.Direction} {myMotorway.Name} {myMotorway.Type} with {myMotorway.Lanes} lanes");
+            Console.WriteLine(myMotorway.ToString());
+
+        }
+    }
+}
