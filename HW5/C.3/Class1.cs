7b554e839b1d20493acce17ec25f8a12b725c263
Terry <tmcdaniel787@gmail.com>
2019-10-10 00:52:30 -0500

    Updated with C3

---
+++ HW5/C.3/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C._3
+{
+    class Money
+    {
+        private decimal _amount;
+        private int _dollars;
+        private int _cents;
+        public void SetAmount(decimal amountAmount)
+        {
+            _amount = amountAmount;
+        }
+        public decimal GetAmount()
+        {
+            return _amount;
+        }
+        public void SetDollars(int dollarAmount)
+        {
+            _dollars=dollarAmount;
+        }
+        public void SetCents(int centsAmount)
+        {
+            _cents = centsAmount;
+        }
+        public int GetDollars()
+        {
+            return _dollars;
+        }
+        public int GetCents()
+        {
+            return _cents;
+        }
+        public string GetCurrency()
+        {
+            int centCurrency = _cents;
+            int quarterCount = 0;
+            int dimeCount = 0;
+            int nickleCount = 0;
+            int pennyCount = 0;
+            while (centCurrency>=25)
+            {
+                quarterCount += 1;
+                centCurrency -= 25;
+            }
+            while (centCurrency>=10 && centCurrency<25)
+            {
+                dimeCount += 1;
+                centCurrency -= 10;
+            }
+            while (centCurrency>=5 && centCurrency<10)
+            {
+                nickleCount += 1;
+                centCurrency -= 5;
+            }
+            while (centCurrency>=1 && centCurrency<5)
+            {
+                pennyCount += 1;
+                centCurrency -= 1;
+            }
+            return ($"Dollars = {_dollars}. Quarters = {quarterCount}. Dimes = {dimeCount}. Nickles = {nickleCount}. Pennys = {pennyCount}.");
+        }
+        string addApprove = "Yes";
+        string subApprove = "Yes";
+        public void AddAmount (decimal addAmount)
+        {
+            if(addAmount>0.00m)
+            {
+                _amount = _amount + addAmount;
+                
+                
+            }
+            else
+            {
+                addApprove = "No";
+            }
+        }
+        public void AddDollars (int addDollars)
+        {
+            if(addApprove=="Yes")
+            {
+                _dollars = _dollars + addDollars;
+            }
+        }
+        public void SubDollars(int subDollars)
+        {
+            if(subApprove=="Yes")
+            {
+                _dollars = _dollars - subDollars;
+            }
+            
+        }
+        public void AddCents(int addCents)
+        {
+            if(addApprove=="Yes")
+            {
+                _cents = _cents + addCents;
+            }
+        }
+        public void SubAmount(decimal subAmount)
+        {
+            if(subAmount>_amount)
+            {
+                Console.WriteLine("The amount is greater than the balance.");
+                subApprove = "No";
+            }
+            else
+            {
+                _amount = _amount - subAmount;
+            }
+        }
+        public void SubCents(int subCents)
+        {
+            if(subApprove=="Yes")
+            {
+                _cents = _cents - subCents;
+            }
+            
+        }
+        public override string ToString()
+        {
+            return ($"The amount is ${_amount}");
+        }
+    }
+}
---
+++ HW5/C.3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money myMoney = new Money();
+            Console.WriteLine($"Initial amount is: {myMoney.GetAmount()}");
+            Console.WriteLine($"Inital dollars is: {myMoney.GetDollars()}");
+            Console.WriteLine($"Initial cents is: {myMoney.GetCents()}");
+            Console.Write("Please enter a full amount dollars and cents. $ ");
+            decimal theAmount = Convert.ToDecimal(Console.ReadLine());
+            myMoney.SetAmount(theAmount);
+            int theDollars = Convert.ToInt32(theAmount);
+            myMoney.SetDollars(theDollars);            
+            int theCents = (int)((theAmount - (int)theAmount) * 100);
+            myMoney.SetCents(theCents);
+            Console.WriteLine($"Updated amount is: {myMoney.GetAmount()}");
+            Console.WriteLine($"Updated dollars is: {myMoney.GetDollars()}");
+            Console.WriteLine($"Updated cents is: {myMoney.GetCents()}");
+            Console.WriteLine($"The currency breakdown is: {myMoney.GetCurrency()}");
+            Console.Write("Would you like to add or subtract from the amount? Y or N ");
+            string decision = Console.ReadLine();
+            if(decision == "Y")
+            {
+                Console.Write("Would you like to add or subtract? ");
+                string addSubtract = Console.ReadLine();
+                if(addSubtract=="add")
+                {
+                    Console.Write("Please input the amount to add. ");
+                    decimal addAmount = Convert.ToDecimal(Console.ReadLine());
+                    myMoney.AddAmount(addAmount);
+                    theDollars= Convert.ToInt32(addAmount);
+                    myMoney.AddDollars(theDollars);
+                    theCents = (int)((addAmount - (int)addAmount) * 100);
+                    myMoney.AddCents(theCents);
+                }
+                else
+                {
+                    Console.Write("Please input the amount to subtract. ");
+                    decimal subAmount = Convert.ToDecimal(Console.ReadLine());
+                    myMoney.SubAmount(subAmount);
+                    theDollars = Convert.ToInt32(subAmount);
+                    myMoney.SubDollars(theDollars);
+                    theCents = (int)((subAmount - (int)subAmount) * 100);
+                    myMoney.SubCents(theCents);
+                }
+
+            }
+            Console.WriteLine($"Updated amount is: {myMoney.GetAmount()}");
+            Console.WriteLine($"Updated dollars is: {myMoney.GetDollars()}");
+            Console.WriteLine($"Updated cents is: {myMoney.GetCents()}");
+            Console.WriteLine($"The currency breakdown is: {myMoney.GetCurrency()}");
+            Console.WriteLine(myMoney.ToString());
+        }
+    }
+}
